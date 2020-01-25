4fb82f495386b3213a0aa1d7a7a83567102fb6b4
Daniel Connolly <djconnolly27@gmail.com>
2020-01-25 16:33:45 -0500

    Adds hello world optimization exercise

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+/* The optimization seems to ignore unused variables and bits of code.
+  Additionally, it removed the "addl" command from the code, which suggests
+  that it is precomputing the output of the addition of 5 + 1 rather than actually allowing
+  the code to run and wasting valuable time and space.
+*/
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    // printf("Hello, World! %i\n", x);
+    int y = x + 1;
+    printf("%i", y);
     return 0;
 }
